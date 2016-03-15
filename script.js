6d1ff704b694e37d3ba540b8791155d4f2e2c690
Freakils <luvo@projectcodex.co>
2016-03-15 13:30:17 +0200

    playing

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 function getRandonNumber(min, max) {
-  return Math.floor(Math.random() * (9 - 1)) + 1;
+  return Math.floor(Math.random() * (3 - 1)) + 1;
 };
-var range = getRandonNumber(1,9);
-document.getElementById("box1").innerHTML = range;
+
+	var random = getRandonNumber(1,3);
+	var randomTwo = getRandonNumber(1,3);
+	var randomThree = getRandonNumber(1,3);
+
+	var box = document.querySelectorAll(".box");
+	// document.getElementById("box1").innerHTML = range;
+
+	for (var count = 1; count < box.length; count++){
+		box[0].innerHTML = random;
+		box[1].innerHTML = randomTwo;
+		box[2].innerHTML = randomThree;
+	};
+

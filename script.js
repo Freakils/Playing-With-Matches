b00f2e55b42f5f770a1eb2c278712f2e3d3687e5
Freakils <luvo@projectcodex.co>
2016-03-18 14:24:42 +0200

    playng

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-function getRandonNumber(min, max) {
+function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (4 - 1)) + 1;
 };
-
-	var random = getRandonNumber(1,4);
-	var randomTwo = getRandonNumber(1,4);
-	var randomThree = getRandonNumber(1,4);
 
 	var box = document.querySelectorAll(".box");
 	// document.getElementById("box1").innerHTML = range;
 
 	for (var count = 0; count < box.length; count++){
-		box[0].innerHTML = random;
-		box[1].innerHTML = randomTwo;
-		box[2].innerHTML = randomThree;
+		box[count].innerHTML = getRandomNumber(1,4);
+	}
+		// box[count+1].innerHTML = getRandomNumber(1,4);
+		// box[2].innerHTML = randomThree;
 
-			if(box[0].innerHTML === box[1].innerHTML && box[0].innerHTML === box[2].innerHTML) {
-				box[0].classList.add("high");
+			if(box[1].innerHTML === box[2].innerHTML) {
 				box[1].classList.add("high");
 				box[2].classList.add("high");
 			}
 
-			// if((keyName === "right") && (trafficLight.color() === "green")){
-			// if(taxiLocationCounter<9){
-			// moveForward();
-			// };
+			if(box[0].innerHTML === box[1].innerHTML) {
+				box[0].classList.add("high");
+				box[1].classList.add("high");
+			}
 
-	};
-
+			if(box[0].innerHTML === box[2].innerHTML) {
+				box[0].classList.add("high");
+				box[2].classList.add("high");
+			}
 
 // function getRandomNumber(min, max) {
 //   return Math.floor(Math.random() * (max - min)) + 1;
@@ -35,23 +33,6 @@ function getRandonNumber(min, max) {
 	
 
 // var box = document.querySelectorAll(".box");
-
-
-// // function updateBlock(){
-// // 	for (var count = 0; count < box.length; count++) {
-// // 		box[count].innerHTML = getRandomNumber(1,3)	
-// // 		box[count].classList.remove('high')
-// // 	}
-// // }
-
-// // function highlightMatching() {
-// // 	for (var x = 0; x < box.length; x++) {
-// // 		if(box[x].innerHTML === box[x+1].innerHTML) {
-// // 			box[x].classList.add('high');
-// // 			box[x].classList.add('high');
-// // 		}
-// // 	}
-// // }
 
 // updateBlock();
 // highlightMatching();

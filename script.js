e6b3245c01bddf1e852d02b713ff11e7e04dd734
Freakils <luvo@projectcodex.co>
2016-03-23 16:50:32 +0200

    playing

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function reloadPage() {
 						box[3].classList.add("high");
 						box[5].classList.add("high");
 						var msg = document.getElementById("paragra");
-							msg.innerHTML ="Row2" + "-" + "two numbers are matching : " + box[5].innerHTML + "'s";
+							msg.innerHTML ="Row2" + "-" + "two numbers are matching : " + box[3].innerHTML + "'s";
 					}
 
 					if(box[3].innerHTML === box[4].innerHTML && box[4].innerHTML === box[5].innerHTML && box[3].innerHTML === box[5].innerHTML) {
@@ -86,13 +86,95 @@ function reloadPage() {
 								box[6].classList.add("high");
 								box[8].classList.add("high");
 								var msg = document.getElementById("paragraph");
-									msg.innerHTML ="Row3" + "-" + "two numbers are matching : " + box[8].innerHTML + "'s";
+									msg.innerHTML ="Row3" + "-" + "two numbers are matching : " + box[6].innerHTML + "'s";
 							}
 
 							if(box[6].innerHTML === box[7].innerHTML && box[7].innerHTML === box[8].innerHTML && box[6].innerHTML === box[8].innerHTML) {
 								var msg = document.getElementById("paragraph");
-									msg.innerHTML ="Row3" + "-" + "three numbers are matching : " + box[5].innerHTML + "'s";
+									msg.innerHTML ="Row3" + "-" + "three numbers are matching : " + box[8].innerHTML + "'s";
 							}
+
+// Column - 1
+	if(box[0].innerHTML === box[3].innerHTML) {
+		box[0].classList.add("high");
+		box[3].classList.add("high");
+		var msg = document.getElementById("col");
+			msg.innerHTML ="Column1" + "-" + "two numbers are matching : " + box[3].innerHTML + "'s";
+	}
+
+	if(box[3].innerHTML === box[6].innerHTML) {
+		box[3].classList.add("high");
+		box[6].classList.add("high");
+		var msg = document.getElementById("col");
+			msg.innerHTML ="Column1" + "-" + "two numbers are matching : " + box[6].innerHTML + "'s";
+	}
+
+	if(box[0].innerHTML === box[6].innerHTML) {
+		box[0].classList.add("high");
+		box[6].classList.add("high");
+		var msg = document.getElementById("col");
+			msg.innerHTML ="Column1" + "-" + "two numbers are matching : " + box[0].innerHTML + "'s";
+	}
+	
+	if(box[0].innerHTML === box[3].innerHTML && box[3].innerHTML === box[6].innerHTML && box[0].innerHTML === box[6].innerHTML) {
+		var msg = document.getElementById("col");
+			msg.innerHTML ="Column1" + "-" + "three numbers are matching : " + box[6].innerHTML + "'s";
+	}
+
+			// Column - 2
+			if(box[1].innerHTML === box[4].innerHTML) {
+				box[1].classList.add("high");
+				box[4].classList.add("high");
+				var msg = document.getElementById("colum");
+					msg.innerHTML ="Column2" + "-" + "two numbers are matching : " + box[4].innerHTML + "'s";
+			}
+
+			if(box[4].innerHTML === box[7].innerHTML) {
+				box[4].classList.add("high");
+				box[7].classList.add("high");
+				var msg = document.getElementById("colum");
+					msg.innerHTML ="Column2" + "-" + "two numbers are matching : " + box[4].innerHTML + "'s";
+			}
+
+			if(box[1].innerHTML === box[7].innerHTML) {
+				box[1].classList.add("high");
+				box[7].classList.add("high");
+				var msg = document.getElementById("colum");
+					msg.innerHTML ="Column2" + "-" + "two numbers are matching : " + box[7].innerHTML + "'s";
+			}
+			
+			if(box[1].innerHTML === box[4].innerHTML && box[4].innerHTML === box[7].innerHTML && box[1].innerHTML === box[7].innerHTML) {
+				var msg = document.getElementById("colum");
+					msg.innerHTML ="Column2" + "-" + "three numbers are matching : " + box[7].innerHTML + "'s";
+			}
+
+					// Column - 3
+					if(box[2].innerHTML === box[5].innerHTML) {
+						box[2].classList.add("high");
+						box[5].classList.add("high");
+						var msg = document.getElementById("column");
+							msg.innerHTML ="Column3" + "-" + "two numbers are matching : " + box[5].innerHTML + "'s";
+					}
+
+					if(box[5].innerHTML === box[8].innerHTML) {
+						box[5].classList.add("high");
+						box[8].classList.add("high");
+						var msg = document.getElementById("column");
+							msg.innerHTML ="Column3" + "-" + "two numbers are matching : " + box[8].innerHTML + "'s";
+					}
+
+					if(box[2].innerHTML === box[8].innerHTML) {
+						box[2].classList.add("high");
+						box[8].classList.add("high");
+						var msg = document.getElementById("column");
+							msg.innerHTML ="Column3" + "-" + "two numbers are matching : " + box[2].innerHTML + "'s";
+					}
+					
+					if(box[2].innerHTML === box[5].innerHTML && box[5].innerHTML === box[8].innerHTML && box[2].innerHTML === box[8].innerHTML) {
+						var msg = document.getElementById("column");
+							msg.innerHTML ="Column3" + "-" + "three numbers are matching : " + box[8].innerHTML + "'s";
+					}
+	
 };
 reloadPage();
 

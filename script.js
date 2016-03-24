f127a7e39bdb95e344495b1dc000486ca4387199
Freakils <luvo@projectcodex.co>
2016-03-24 11:15:08 +0200

    playing

--- script.js
+++ script.js
@@ -174,7 +174,61 @@ function reloadPage() {
 						var msg = document.getElementById("column");
 							msg.innerHTML ="Column3" + "-" + "three numbers are matching : " + box[8].innerHTML + "'s";
 					}
-	
+
+// Diagonal -1
+if(box[0].innerHTML === box[4].innerHTML) {
+	box[0].classList.add("high");
+	box[4].classList.add("high");
+	var msg = document.getElementById("dia");
+		msg.innerHTML ="Diagonal1" + "-" + "two numbers are matching : " + box[0].innerHTML + "'s";
+}
+
+if(box[4].innerHTML === box[8].innerHTML) {
+	box[4].classList.add("high");
+	box[8].classList.add("high");
+	var msg = document.getElementById("dia");
+		msg.innerHTML ="Diagonal1" + "-" + "two numbers are matching : " + box[4].innerHTML + "'s";
+}
+
+if(box[0].innerHTML === box[8].innerHTML) {
+	box[0].classList.add("high");
+	box[8].classList.add("high");
+	var msg = document.getElementById("dia");
+		msg.innerHTML ="Diagonal1" + "-" + "two numbers are matching : " + box[8].innerHTML + "'s";
+}
+					
+if(box[0].innerHTML === box[4].innerHTML && box[4].innerHTML === box[8].innerHTML && box[0].innerHTML === box[8].innerHTML) {
+	var msg = document.getElementById("dia");
+		msg.innerHTML ="Diagonal1" + "-" + "three numbers are matching : " + box[8].innerHTML + "'s";
+}
+
+		// Diagonal -2
+		if(box[2].innerHTML === box[4].innerHTML) {
+			box[2].classList.add("high");
+			box[4].classList.add("high");
+			var msg = document.getElementById("diago");
+				msg.innerHTML ="Diagonal2" + "-" + "two numbers are matching : " + box[2].innerHTML + "'s";
+		}
+
+		if(box[4].innerHTML === box[6].innerHTML) {
+			box[4].classList.add("high");
+			box[6].classList.add("high");
+			var msg = document.getElementById("diago");
+				msg.innerHTML ="Diagonal2" + "-" + "two numbers are matching : " + box[4].innerHTML + "'s";
+		}
+
+		if(box[2].innerHTML === box[6].innerHTML) {
+			box[2].classList.add("high");
+			box[6].classList.add("high");
+			var msg = document.getElementById("diago");
+				msg.innerHTML ="Diagonal2" + "-" + "two numbers are matching : " + box[6].innerHTML + "'s";
+		}
+							
+		if(box[2].innerHTML === box[4].innerHTML && box[4].innerHTML === box[6].innerHTML && box[2].innerHTML === box[6].innerHTML) {
+			var msg = document.getElementById("diago");
+				msg.innerHTML ="Diagonal2" + "-" + "three numbers are matching : " + box[8].innerHTML + "'s";
+		}			
+
 };
 reloadPage();
 
